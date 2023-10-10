deae884566195f2e58fbc0ce5638e576ffe1d691
Ian Vega <lan@ciencias.unam.mx>
2023-10-10 14:01:18 -0600

    Delete services directory

--- services/user.js
+++
@@ -1,8 +0,0 @@
-const User = require('../models/users');
-
-/*
-exports.createUser = (data)=> User.create(data);
-*/
-exports.createUser =  function(data){
-    return User.create(data); //crear un usuario en la bd
-}
