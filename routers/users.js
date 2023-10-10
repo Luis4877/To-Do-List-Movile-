7492f8595a086f5e811b25963e6a3d8ef85a947e
Ian Vega <lan@ciencias.unam.mx>
2023-10-10 14:01:12 -0600

    Delete routers directory

--- routers/users.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createUser } = require("../controllers/users");
-//crear usuario
-router.get("/createUser", createUser);
-
-//exportamos el router para poder unirlo en la aplicacion principal
-
-module.exports = router;
